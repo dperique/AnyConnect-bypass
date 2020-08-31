e4d7c3fe2572e876d9d259b3223263b0a00cbb09
Dennis Periquet <Dennis.Periquet@ibm.com>
2020-08-31 10:43:46 -0400

    Test for 32 bit networks case

--- python/test_transform_to_routes.py
+++ python/test_transform_to_routes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 import unittest
 
 import transform_to_routes as trans
@@ -15,30 +16,52 @@ Run like this:
 
 class Test_Transform_to_routes(unittest.TestCase):
   def test_basics(self):
-    """
-    Test basic run use case for both modes.
-    """
-    for aMode in trans.mode_list:
-        self.assertEqual(trans.transform_to_routes("sampleStatFile.txt", "192.168.1.1", aMode)[0], 0)
+      """
+      Test basic run use case for both modes.
+      """
+      for aMode in trans.mode_list:
+          self.assertEqual(trans.transform_to_routes("sampleStatFile.txt", "192.168.1.1", aMode)[0], 0)
 
   def test_gateway(self):
-    """
-    Test that the given GW shows up in the routes for both modes.
-    """
-    anIP = "192.168.1.100"
+      """
+      Test that the given GW shows up in the routes for both modes.
+      """
+      anIP = "192.168.1.100"
 
-    for aMode in trans.mode_list:
+      for aMode in trans.mode_list:
 
-        tup = trans.transform_to_routes("sampleStatFile.txt", anIP, aMode)
+          tup = trans.transform_to_routes("sampleStatFile.txt", anIP, aMode)
     
-        for line in tup[1]:
-            if anIP in line:
-                break
-        else:
-            print(f"The GW of '{anIP}' is not in the '{aMode}' route commands")
-            self.assertTrue(False)
+          for line in tup[1]:
+              if anIP in line:
+                  break
+          else:
+              print(f"The GW of '{anIP}' is not in the '{aMode}' route commands")
+              self.assertTrue(False)
 
-        self.assertEqual(tup[0], 0)
+          self.assertEqual(tup[0], 0)
+
+  def test_32_bit_linux(self):
+      """
+      Test that we handle 32 bit networks using correct syntax.
+      The sample file contains two 32 bit networks and should look like:
+        route add -host x.x.x.x x.x.x.x
+      """
+      tup = trans.transform_to_routes("sampleStatFile.txt", "192.168.1.131", "linux")
+      host_lines = [line for line in tup[1] if re.search("route add -host \d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}", line)]
+      self.assertEqual(tup[0], 0)
+      self.assertEqual(len(host_lines), 2)
+
+  def test_32_bit_macOS(self):
+      """
+      Test that we handle 32 bit networks using correct syntax.
+      The sample file contains two 32 bit networks and should look like:
+        route -n add x.x.x.x/32 x.x.x.x
+      """
+      tup = trans.transform_to_routes("sampleStatFile.txt", "192.168.1.131", "macOS")
+      host_lines = [line for line in tup[1] if re.search("route -n add \d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}/32", line)]
+      self.assertEqual(tup[0], 0)
+      self.assertEqual(len(host_lines), 2)
 
 if __name__ == '__main__':
     unittest.main()
